d85b3694864167f1def2f4845c7123a3c72f5b63
Supriya222 <59147074+Supriya222@users.noreply.github.com>
2021-07-08 20:27:25 +0530

    Update main.dart

--- Calculator/lib/main.dart
+++ Calculator/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import './view/calculator_app.dart';
 import './provider/calculate.dart';
 
+// Created a simple calculator app (using MVP model in flutter), that can only perform any one of the operation(from +,-,/,*,%)
+//in a single line at one time . Working on performing multiple operation in same line at a time.
 void main() {
   runApp(
     MultiProvider(providers: [
